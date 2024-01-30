b96faefcf26596941c6a2881f92d827f316caafc
simonexlue <simonexlue@gmail.com>
2024-01-29 19:04:17 -0800

    Initial commit

---
+++ app/src/main/java/com/example/quenchquest/LoginAndRegister/Login.java
@@ -0,0 +1,16 @@
+package com.example.quenchquest.LoginAndRegister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quenchquest.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
